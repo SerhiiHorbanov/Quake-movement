430de6360b387ca3d6b8b2b073ebe9fc34998170
Serhii <serikgorbanev@gmail.com>
2023-11-11 16:18:49 +0100

    Made air movement. Added Physic material to both player and ground.
    Added terrain to SampleScene for testing.

--- MovementProject/Assets/Scripts/Player/PlayerMovementWalk.cs
+++ MovementProject/Assets/Scripts/Player/PlayerMovementWalk.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovementWalk : MonoBehaviour
 {
     [SerializeField] float maxSpeed;
+    [SerializeField] float maxAirSpeed;
     [SerializeField] float maxAccel;
     [SerializeField] float frictionMultiplier;
 
@@ -19,33 +20,29 @@ public class PlayerMovementWalk : MonoBehaviour
             => rigidBody.velocity = value;
     }
 
-    private void WalkOnGround(Vector2 direction)
+    public void Walk(Vector2 direction)
     {
+        if (groundCheck == null)
+            return;
+
         direction.Normalize();
 
         Vector2 horizontalVelocity = new Vector2(Velocity.x, Velocity.z);
 
         float speed = Vector2.Dot(horizontalVelocity, direction);//speed in the direction of direction vector
 
-        float additionalSpeed = Mathf.Clamp(maxSpeed - speed, 0, maxAccel);
+        float currentMaxSpeed = groundCheck.isOnGround ? maxSpeed : maxAirSpeed;
+
+        float additionalSpeed = Mathf.Clamp(currentMaxSpeed - speed, 0, maxAccel);
 
         Vector3 additionalVelocity = new Vector3(direction.x * additionalSpeed, 0, direction.y * additionalSpeed);
 
         Velocity += additionalVelocity;
     }
 
-    public void Walk(Vector2 direction)
-    {
-        if (groundCheck == null)
-            return;
-
-        if(groundCheck.isOnGround)
-            WalkOnGround(direction);
-    }
-
     private void FixedUpdate()
     {
         if (groundCheck.isOnGround)
-            Velocity = new Vector3(rigidBody.velocity.x * frictionMultiplier, rigidBody.velocity.y, rigidBody.velocity.z * frictionMultiplier);
+            Velocity = new Vector3(Velocity.x * frictionMultiplier, Velocity.y, Velocity.z * frictionMultiplier);
     }
 }
